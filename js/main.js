6eaf75eee65aab557d79a55add9a11e5d1c52dc5
Ildar Gubaidullin <tm-542-tm@mail.ru>
2020-04-21 21:17:34 +0300

    Выполнил ДЗ.

--- js/main.js
+++ js/main.js
@@ -3,40 +3,26 @@ const products = [
   {id: 2, title: 'Mouse', price: 1500},
   {id: 3, title: 'Keyboard', price: 5000},
   {id: 4, title: 'Gamepad', price: 4500},
+  {id: 4, title: 'Monitor'},
 ];
 
-const renderProducts = (products) => {
-  return products.map((product) => {
-    return `<div class="product-item">
+class ProductsRender {
+
+  static renderProducts(products){
+    return products.map((product) => {
+      return `<div class="product-item">
             <h3>${product.title}</h3>
             <img class="image" src="http://placehold.it/250"/>
-            <p> Цена: ${product.price} рублей </p>
+            <p> Цена: ${product.price  || 'уточняется'} </p>
             <button class="by-btn">Добавить в корзину</button>
           </div>`
-  }).join('') /// ЗАПЯТЫЕ УБРАЛ ИСПОЛЬЗУЯ МЕТОД jOIN()
+    }).join('') /// ЗАПЯТЫЕ УБРАЛ ИСПОЛЬЗУЯ МЕТОД jOIN()
+  }
+
+  static viewProducts(selector, html){
+    document.querySelector(selector).innerHTML = html
+  }
 }
 
-// const renderProduct = (title, price) => {
-//   return `<div class="product-item">
-//             <h3>${title}</h3>
-//             <img class="image" src="http://placehold.it/250"/>
-//             <p> Цена: ${price} рублей </p>
-//             <button class="by-btn">Добавить в корзину</button>
-//           </div>`;
-// };
-//
-// const renderProducts = (list) => {
-//   // const productList = list.map((good) => {
-//   //   return renderProduct(good.title, good.price);
-//   // });
-//   const productList = [];
-//   list.forEach(good => {
-//     productList.push(renderProduct(good.title, good.price));
-//   });
-//   console.log(productList);
-//   document.querySelector('.products').innerHTML = productList.join(' ');
-// };
-
-//console.log(renderProducts(products));
-
-document.querySelector('.products').innerHTML = renderProducts(products)
+const html = ProductsRender.renderProducts(products)
+ProductsRender.viewProducts('.products', html)
